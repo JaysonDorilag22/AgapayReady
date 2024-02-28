b83dba5c9a12745afa0f09c9b672cccb97122e8a
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-02-28 15:10:53 +0800

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from './redux/store'
+import { persistor, store } from './store.js'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import userSlice from './user/userSlice.js';
+import userSlice from './redux/user/userSlice.js';
 
 const rootReducer = combineReducers({
   user: userSlice,
