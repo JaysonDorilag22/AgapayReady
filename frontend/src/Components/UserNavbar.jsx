f75820e648b525bf3148d217e53c66fcd45d6150
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-02-28 23:12:34 +0800

    Changes

--- frontend/src/Components/UserNavbar.jsx
+++ frontend/src/Components/UserNavbar.jsx
@@ -1,51 +1,62 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import AgapayReadylogo from "../assets/services/AgapayReadylogo.png";
-import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Dialog, Disclosure, Popover} from "@headlessui/react";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
 
-function MobileMenu({ onClose }) {
-  return (
-    <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-      <div className="flex items-center justify-between">
-        <Link to="/" className="-m-1.5 p-1.5">
-          <span className="sr-only">AgapayReadylogo</span>
-          <img className="h-10 w-auto" src={AgapayReadylogo} alt="AgapayReadylogo" />
-        </Link>
-        <button
-          type="button"
-          className="-m-2.5 rounded-md p-2.5 text-gray-700"
-          onClick={onClose}
-        >
-          <span className="sr-only">Close menu</span>
-          <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-      <div className="mt-6 flow-root">
-        <div className="-my-6 divide-y divide-gray-500/10">
-          <div className="space-y-2 py-6">
-            <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-              Features
-            </Link>
-            <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-              Marketplace
-            </Link>
-            <Link to="/" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-              Company
-            </Link>
-          </div>
-          <div className="py-6">
-            <Link to="/login" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-              Log in
-            </Link>
-            <Link to="/register" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-              Register
-            </Link>
-          </div>
-        </div>
-      </div>
-    </Dialog.Panel>
-  );
+const products = [
+  {
+    name: "Analytics",
+    description: "Get a better understanding of your traffic",
+    to: "/",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Engagement",
+    description: "Speak directly to your customers",
+    to: "/",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "Security",
+    description: "Your customers’ data will be safe and secure",
+    to: "/",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Integrations",
+    description: "Connect with third-party tools",
+    to: "/",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "Automations",
+    description: "Build strategic funnels that will convert",
+    to: "/",
+    icon: ArrowPathIcon,
+  },
+];
+
+const callsToAction = [
+  { name: "Watch demo", to: "/", icon: PlayCircleIcon },
+  { name: "Contact sales", to: "/", icon: PhoneIcon },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function UserNavbar() {
@@ -53,11 +64,14 @@ export default function UserNavbar() {
 
   return (
     <div className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-10 w-auto" src={AgapayReadylogo} alt="AgapayReadylogo" />
+            <img className="h-10 w-auto" src={AgapayReadylogo} alt="" />
           </Link>
         </div>
         <div className="flex lg:hidden">
@@ -70,6 +84,32 @@ export default function UserNavbar() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Link
+            to="/"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Features
+          </Link>
+          <Link
+            to="/"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Marketplace
+          </Link>
+          <Link
+            to="/evacuation/guidelines"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Evacuation Guidelines
+          </Link>
+          <Link
+            to="/blog"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Blog
+          </Link>
+        </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Link
             to="/login"
@@ -83,10 +123,103 @@ export default function UserNavbar() {
         as="div"
         className="lg:hidden"
         open={mobileMenuOpen}
-        onClose={() => setMobileMenuOpen(false)}
+        onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <MobileMenu onClose={() => setMobileMenuOpen(false)} />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link to="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img className="h-10 w-auto" src={AgapayReadylogo} alt="" />
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        Product
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products, ...callsToAction].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as={Link}
+                            to={item.to}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <Link
+                  to="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </Link>
+                <Link
+                  to="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </Link>
+                <Link
+                  to="/"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </Link>
+                <Link
+                  to="/evacuation/guidelines"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Evacuation Guidelines
+                </Link>
+                <Link
+                  to="/blog"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Blog
+                </Link>
+              </div>
+              <div className="py-6">
+                <Link
+                  to="/login"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </Link>
+                <Link
+                  to="/register"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Register
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
       </Dialog>
     </div>
   );
