ee18581008fb060802f0e5f34b1aef5f8d858b56
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-02-28 20:04:08 +0800

    Changes

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Vite from "../assets/services/vite.png";
+import vite from "../assets/services/vite.png";
 
 export default function Footer() {
   return (
     <div className="footer p-10 bg-base-200 text-base-content">
       <aside>
-        <img src={Vite} alt="Vite Logo" className="h-20 w-auto" />
+        <img src={vite} alt="Vite Logo" className="h-20 w-auto" />
         <p>AgapayReady<br/>Emergency Response App since 2024</p>
       </aside> 
       <nav>
