ee6f713d6bda6a5edf6b0f84fbad0b1143047681
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-02-28 15:26:43 +0800

    changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,8 +2,8 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-//this is the right path
 import userSlice from './redux/user/userSlice';
+//this is the right path
 
 const rootReducer = combineReducers({
   user: userSlice,
