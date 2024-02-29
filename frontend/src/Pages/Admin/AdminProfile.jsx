5b975cd129958a37c7d572c053ae9f948d6306f1
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-02-29 13:55:59 +0800

    Changes

--- frontend/src/Pages/Admin/AdminProfile.jsx
+++ frontend/src/Pages/Admin/AdminProfile.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useSelector } from "react-redux";
 export default function AdminProfile() {
-  const { currentUser } = useSelector((state) => state.user);
 
   return (
     <div>
