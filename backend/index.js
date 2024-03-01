4253eb82d68f9564d9107d046fe2e06aa1f2968c
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-01 21:01:21 +0800

    Update

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://agapayready-1.onrender.com",
     methods: ["GET", "POST"]
   }
 });
@@ -34,7 +34,11 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
