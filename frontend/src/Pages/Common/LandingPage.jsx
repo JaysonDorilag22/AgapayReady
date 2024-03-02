365ba08d9d0dbc0f99089b8c927c7573902ad375
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-02 21:14:02 +0800

    Deployment Frontend issue

--- frontend/src/Pages/Common/LandingPage.jsx
+++ frontend/src/Pages/Common/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ShuffleHero from "./ShuffleHero";
-import Features from "../../components/Features";
-import EarthquakeGuidelines from "./EarthquakeGuidelines";
+import Features from "./Features";
 import Teams from "./Teams";
 import FAQSection from "./FAQSection";
 export default function landingPage() {
