3adc46781a1a24c67a230d34063b9c9bd9eb099e
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-04 23:55:56 +0800

    move modal

--- frontend/src/Pages/Common/GuidelinesbyCategory.jsx
+++ frontend/src/Pages/Common/GuidelinesbyCategory.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Modal from '../../components/Modal';
+import Modal from './Modal';
 import axios from 'axios';
 import { FaSearch } from "react-icons/fa";
 
