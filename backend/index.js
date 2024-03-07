0c3eec279f75d001f544afa8adaa6018705c5881
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-07 23:06:15 +0800

    vercel

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: "https://agapay-ready.vercel.app",
     methods: ["GET", "POST"]
   }
 });
