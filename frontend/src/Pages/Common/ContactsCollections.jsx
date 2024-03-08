9b26efbf935841d8d6b74fd09c576b4719c2f9c0
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-08 09:28:58 +0800

    Loader Contacts

--- frontend/src/Pages/Common/ContactsCollections.jsx
+++ frontend/src/Pages/Common/ContactsCollections.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import Loader from '../../components/Loader';
+import Loader from "../../components/Loader"
 
 export default function ContactsCollection() {
   const [categories, setCategories] = useState([]);
