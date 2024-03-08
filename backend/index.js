6e1028a6115d37d5ad0f3789ab2ad94e89f69f22
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-08 15:06:48 +0800

    check

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://agapayready.onrender.com",
   credentials: true}))
 
 app.use(express.json());
