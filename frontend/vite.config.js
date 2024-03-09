7b2424e79481bc3b363e8e15fdfc52afa90e502f
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-10 07:37:54 +0800

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Load environment variables from .env file
-dotenv.config();
-
-// Get environment variables
-const backendUrl = process.env.VITE_BACKEND_URL;
-
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "https://agapayready.onrender.com",
+        target: 'http://localhost:4000',
         secure: false,
-        changeOrigin: true,
       },
     },
   },
   plugins: [react()],
-});
+})
