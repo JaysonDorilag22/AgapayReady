4bc66a91f4ac9eb546e477fb9b8f9a93e6500d24
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-10 07:11:27 +0800

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://localhost:4000', // Proxy for development
         secure: false,
+        changeOrigin: true, // Necessary for the correct handling of the 'Host' header
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove '/api' prefix
       },
     },
   },
   plugins: [react()],
-})
+});
