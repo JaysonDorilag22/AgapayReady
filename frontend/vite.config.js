0912d633afeb8766f220b5627516045c98ac261e
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-10 07:32:42 +0800

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: backendUrl,
+        target: "https://agapayready.onrender.com",
         secure: false,
         changeOrigin: true,
       },
