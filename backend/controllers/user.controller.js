8be6c7c79d5b59c2cf67b7e346a0a5460888baf2
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-10 14:52:49 +0800

    forgot password backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -172,3 +172,87 @@ export const getAllUsers = async (req, res, next) => {
     errorHandler(error);
   }
 };
+
+
+export const forgotPassword = async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const resetToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Password Reset",
+      html: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Password Reset</title>
+        </head>
+        <body style="font-family: Arial, sans-serif;">
+
+          <!-- Logo Section -->
+          <div style="text-align: center;">
+            <img src="http://localhost:5173/src/assets/services/vite.png" alt="Your Logo" style="max-width: 200px; margin-bottom: 20px;">
+          </div>
+
+          <!-- Email Content -->
+          <div style="max-width: 600px; margin: 0 auto;">
+            <p>Dear User,</p>
+            <p>We hope this email finds you well. You are receiving this message because a request has been made to reset the password for your account.</p>
+            <p>To proceed with the password reset, please click on the following link or copy-paste it into your browser:</p>
+            <p><a href="${process.env.CLIENT_URL}/reset-password/${resetToken}">Reset Password</a></p>
+            <p>If you did not initiate this request, you can safely disregard this email. Your password will remain unchanged.</p>
+            <p>Thank you for your attention.</p>
+            <p>Best regards,</p>
+            <p>Your Application Team</p>
+          </div>
+
+        </body>
+        </html>
+      `,
+    });
+
+    res.status(200).json({
+      message: "Password reset instructions have been sent to your email",
+    });
+  } catch (error) {
+    errorHandler(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const resetPassword = async (req, res, next) => {
+  const { resetToken } = req.params;
+  const { password } = req.body;
+
+  try {
+    const decodedToken = jwt.verify(resetToken, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 12);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Password reset successful" });
+  } catch (error) {
+    errorHandler(error);
+    res.status(400).json({ error: "Invalid or expired token" });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import multer from 'multer';
-import { register, login, logout, updateUser, confirmEmail, getAllUsers } from '../controllers/user.controller.js';
+import { register, login, logout, updateUser, confirmEmail, getAllUsers, resetPassword, forgotPassword } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verify.js';
-import { isAdmin } from '../middleware/isAdmin.js'
+import { isAdmin } from '../middleware/isAdmin.js';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' }); 
@@ -10,12 +10,13 @@ const upload = multer({ dest: 'uploads/' });
 router.post('/register', upload.single('avatar'), register); 
 router.put('/confirm/:userId/:token', confirmEmail); 
 
-router.post('/login', login);
-router.get('/logout', logout);
-router.get('/users', verifyToken, isAdmin, getAllUsers);
+router.post('/login', login); 
+router.get('/logout', logout); 
+router.get('/users', verifyToken, isAdmin, getAllUsers); 
 
-
-// Modify this route to accept the 'coverPhoto' field
 router.put('/update/:userId', verifyToken, upload.single('coverPhoto'), updateUser); 
+
+router.post('/forgot-password', forgotPassword); 
+router.put('/reset-password/:resetToken', resetPassword); 
 
 export default router;
