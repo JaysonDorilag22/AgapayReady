56f0aad88a5a8b13b1ea2dc9b5acd93cec497206
JaysonDorilag22 <jaysondorilag1@gmail.com>
2024-03-12 08:07:40 +0800

    fix

--- frontend/src/Pages/Common/EvacuationGuidelines.jsx
+++ frontend/src/Pages/Common/EvacuationGuidelines.jsx
@@ -54,16 +54,26 @@ const EvacuationGuidelines = () => {
           </div>
           <div className="flex flex-1 items-center justify-center mb-10">
             <div className="w-full max-w-lg">
-              <div className="flex items-center justify-between mb-5">
+              <form className="sm:flex sm:items-center">
                 <input
-                  type="text"
+                  id="q"
+                  name="q"
+                  className="inline w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-3 leading-5 placeholder-gray-500 focus:border-red-500 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-red-500 sm:text-sm"
+                  placeholder="Search Guideline . . ."
+                  type="search"
+                  autoFocus=""
                   value={searchQuery}
                   onChange={handleSearchChange}
-                  placeholder="Search Guidelines"
-                  className="p-2 border border-gray-300 rounded-md w-full"
                 />
-                <FaSearch className="ml-2 text-gray-600" />
-              </div>
+                <button
+                  type="submit"
+                  className="mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                >
+                  <FaSearch />
+                </button>
+              </form>
+            </div>
+          </div>
               <div className="grid gap-4 grid-cols-2 lg:grid-cols-3">
                 {filteredGuidelines.map((guideline) => (
                   <Card
@@ -74,8 +84,8 @@ const EvacuationGuidelines = () => {
                   />
                 ))}
               </div>
-            </div>
-          </div>
+            {/* </div>
+          </div> */}
         </>
       )}
     </div>
